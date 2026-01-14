3b72158a79572e0445edfbd81a0041e61341422e
Jose-ant-1 <jgonsol1510@g.educaand.es>
2026-01-14 11:49:21 +0100

    estilo aplicado, continuar por el ejercicio 4

--- src/app/ex03/ex03.component.ts
+++ src/app/ex03/ex03.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-ex03',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass, NgIf
+  ],
   templateUrl: './ex03.component.html',
   styleUrl: './ex03.component.css'
 })
+
 export class Ex03Component {
   n: number = 0;
-
+  colorNota: String = '';
   constructor() { }
+
+  comprobarNumero () {
+    if (this.n < 5) {
+      this.colorNota = 'rojo';
+    } else if (this.n < 9){
+      this.colorNota = 'verde';
+    } else {
+      this.colorNota = 'azul';
+    }
+  }
 
   ngOnInit(): void {
     this.n = Math.floor(Math.random() * 10 + 1);
+    this.comprobarNumero();
   }
 }
